4c286634734a9a725bde8e2a6ad680eeb58cf955
terrier989 <terrier989@gmail.com>
2019-09-08 15:55:23 +0000

    Refactored a file into multiple smaller files.

---
+++ lib/src/html/driver/browser_implementation.dart
@@ -0,0 +1,143 @@
+// Copyright 2019 terrier989@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+part of universal_html.internal;
+
+// -----------------------------------------------------------------------------
+// This API is:
+//  * Part of library '/src/html.dart' so we have access to private fields/methods.
+//  * Hidden in library '/html.dart'
+//  * Exported by library '/driver.dart'
+// -----------------------------------------------------------------------------
+/// Implementation of browser functions. The instance is usually obtained via
+/// [HtmlDriver.browserImplementation].
+///
+/// [BrowserImplementation] contains functionality such as:
+///   * Class factories (e.g. [newWindow])
+///   * Special event handling code (e.g. [handleFileUploadInputElementClick])
+///
+/// Sometimes it may not clear whether functionality should be written in
+/// [HtmlDriver] or [BrowserImplementation]. The general rule is that if
+/// developers may override a method, it should be in [BrowserImplementation.
+class BrowserImplementation {
+  final HtmlDriver htmlDriver;
+
+  BrowserImplementation(this.htmlDriver);
+
+  /// Called by [EventTarget] when default behavior of an event should occur.
+  ///
+  /// In other words:
+  ///   * Capturing and bubbling phases of event handling are already done when
+  ///     this method is invoked.
+  ///   * This method is not invoked if [Event.preventDefault] was called by
+  ///     an event handler.
+  ///
+  /// The default implementation handles the following events:
+  ///   * [InputElement] clicks
+  void handleEventDefault(EventTarget eventTarget, Event event) {
+    switch (event.type) {
+      case "click":
+        if (eventTarget is InputElement) {
+          switch (eventTarget.type.toLowerCase()) {
+            case "file":
+              handleFileUploadInputElementClick(eventTarget, event);
+              break;
+
+            case "reset":
+              eventTarget.form?.reset();
+              break;
+
+            case "submit":
+              eventTarget.form?.submit();
+              break;
+
+            default:
+              eventTarget.focus();
+              break;
+          }
+        }
+        break;
+    }
+  }
+
+  /// Called when [FileUploadInputElement] is clicked.
+  void handleFileUploadInputElementClick(
+      FileUploadInputElement element, Event event) {
+    throw UnimplementedError();
+  }
+
+  /// Called by [FormElement.reset].
+  void handleFormReset(FormElement element) {
+    throw UnimplementedError();
+  }
+
+  /// Called by [FormElement.submit].
+  void handleFormSubmit(FormElement element) {
+    throw UnimplementedError();
+  }
+
+  ApplicationCache newApplicationCache() => throw UnimplementedError();
+
+  CanvasRenderingContext2D newCanvasRenderingContext2D(CanvasElement element) =>
+      throw UnimplementedError();
+
+  /// Constructs 'dart:html' _window.console_.
+  Console newConsole() => Console._();
+
+  /// Constructs 'dart:html' _navigator.geolocation_.
+  Geolocation newGeolocation() => Geolocation._();
+
+  /// Constructs 'dart:html' _window.history_.
+  History newHistory() => History._();
+
+  /// Constructs HTTP client used by 'dart:html' APIs that require network
+  /// access.
+  ///
+  /// Examples of such APIs:
+  ///   * [HttpClient]
+  ///   * [EventSource]
+  io.HttpClient newHttpClient() {
+    final httpClient = io.HttpClient();
+    httpClient.userAgent = htmlDriver.userAgent.string;
+    return httpClient;
+  }
+
+  /// Constructs 'dart:html' _window.location_.
+  Location newLocation() => Location._(htmlDriver);
+
+  /// Constructs 'dart:html' _navigator_.
+  Navigator newNavigator() => Navigator._(htmlDriver);
+
+  /// Constructs 'dart:html' _window.console_.
+  RenderData newRenderData(Element element) => RenderData();
+
+  /// Constructs 'dart:html' _window.storage_ / _window.sessionStorage_.
+  Storage newStorage({bool sessionStorage = false}) =>
+      Storage._(htmlDriver.window);
+
+  /// Constructs 'dart:html' _WebSocket_.
+  WebSocket newWebSocket(String url, [Object protocols]) {
+    throw UnimplementedError();
+  }
+
+  /// Constructs 'dart:html' _window_.
+  Window newWindow() => Window._(htmlDriver);
+
+  /// Called by [window.windowRequestFileSystem]. Default implementation throws
+  /// [UnimplementedError].
+  Future<FileSystem> windowRequestFileSystem(int size,
+      {bool persistent = false}) {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/src/html/driver/browser_implementation_utils.dart
@@ -0,0 +1,148 @@
+// Copyright 2019 terrier989@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+part of universal_html.internal;
+
+/// Provides access to private fields of DOM classes.
+class BrowserImplementationUtils {
+  BrowserImplementationUtils._();
+
+  static Future<Uint8List> getBlobData(Blob blob) {
+    if (blob is BlobBase) {
+      return blob.toBytesFuture();
+    }
+    throw ArgumentError.value(blob);
+  }
+
+  static RenderData getElementRenderData(Element element) {
+    return element._renderDataField;
+  }
+
+  static Comment newComment(Document ownerDocument, String value) {
+    return Comment._(ownerDocument, value);
+  }
+
+  static Coordinates newCoordinates({
+    num accuracy = 0.0,
+    num altitude = 0.0,
+    num altitudeAccuracy = 0.0,
+    num heading = 0.0,
+    num latitude = 0.0,
+    num longitude = 0.0,
+    num speed = 0.0,
+  }) {
+    return Coordinates._(
+      accuracy: accuracy,
+      altitude: altitude,
+      altitudeAccuracy: altitudeAccuracy,
+      heading: heading,
+      latitude: latitude,
+      longitude: longitude,
+      speed: speed,
+    );
+  }
+
+  static DocumentFragment newDocumentFragment(Document ownerDocument) {
+    return DocumentFragment._(ownerDocument);
+  }
+
+  static _DocumentType newDocumentType(Document ownerDocument, String value) {
+    return _DocumentType._(ownerDocument, value);
+  }
+
+  static Element newElementWithInternalTag(Document ownerDocument, String name,
+      [String typeExtension]) {
+    return Element.internalTag(ownerDocument, name, typeExtension);
+  }
+
+  static Element newElementWithInternalTagNS(
+      Document ownerDocument, String namespaceUri, String name,
+      [String typeExtension]) {
+    return Element.internalTagNS(
+        ownerDocument, namespaceUri, name, typeExtension);
+  }
+
+  static ErrorEvent newErrorEvent(
+      {int colno, Object error, String filename, int lineno, String message}) {
+    return ErrorEvent._(
+      colno: colno,
+      error: error,
+      filename: filename,
+      lineno: lineno,
+      message: message,
+    );
+  }
+
+  static Geoposition newGeoposition({Coordinates coords, int timestamp}) {
+    return Geoposition._(coords: coords, timestamp: timestamp);
+  }
+
+  static HtmlDocument newHtmlDocument({
+    @required HtmlDriver htmlDriver,
+    @required String contentType,
+    @required bool filled,
+    String origin,
+  }) {
+    return HtmlDocument._(
+      htmlDriver: htmlDriver,
+      contentType: contentType,
+      filled: filled,
+      origin: origin,
+    );
+  }
+
+  static ProcessingInstruction newProcessingInstruction(Document ownerDocument,
+      {StyleSheet sheet, String target}) {
+    return ProcessingInstruction._(
+      ownerDocument,
+      sheet: sheet,
+      target: target,
+    );
+  }
+
+  static Text newText(Document ownerDocument, String value) {
+    return Text._(ownerDocument, value);
+  }
+
+  static UnknownElement newUnknownElement(
+      Document ownerDocument, String namespaceUri, String tag) {
+    return UnknownElement._(ownerDocument, namespaceUri, tag);
+  }
+
+  static XmlDocument newXmlDocument({
+    @required HtmlDriver htmlDriver,
+    String contentType = "text/xml",
+    String origin,
+  }) {
+    return XmlDocument._(
+      htmlDriver: htmlDriver,
+      contentType: contentType,
+      origin: origin,
+    );
+  }
+
+  /// Produces string representation of DOM tree.
+  static String nodeToString(Node node) {
+    if (node == null) {
+      return "";
+    }
+    final sb = StringBuffer();
+    _printNode(sb, _getPrintingFlags(node), node);
+    return sb.toString();
+  }
+
+  static void setElementRenderData(Element element, RenderData renderData) {
+    element._renderDataField = renderData;
+  }
+}
